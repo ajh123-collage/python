e64cd2b49564e8eb101347f44871ba40617eb2a7
ajh123 <41990982+ajh123@users.noreply.github.com>
2022-12-02 12:14:54 +0000

    use a list

--- other/15.py
+++ other/15.py
@@ -5,17 +5,32 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         # just send back the same data, but upper-cased
-        msg = "<h1>404 Not Found</h1>"
-        msg += "<p>The server did not find the file requested.</p><hr><center><p>pyServe 0.0.1</p></center>"
+        msg = []
+        msg.append("<html> lang=\"en\"")
+        msg.append("    <head>")
+        msg.append("        <title>404 Not Found</title>")
+        msg.append("    </head>")
+        msg.append("    <body>")
+        msg.append("        <h1>404 Not Found</h1>")
+        msg.append("        <p>The server did not find the file requested.</p>")
+        msg.append("        <hr>")
+        msg.append("        <center>")
+        msg.append("            <p>pyServe 0.0.1</p>")
+        msg.append("        </center>")
+        msg.append("    </body>")
+        msg.append("</html>")
+
+        msg_raw = ''.join(f'{v}\r\n' for v in msg)
+
         response_headers = {
             'Content-Type': 'text/html; encoding=utf8',
-            'Content-Length': len(msg),
+            'Content-Length': len(msg_raw),
             'Connection': 'close',
         }
 
-        response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
+        response_headers_raw = ''.join(f'{k}: {v}\r\n' for k, v in response_headers.items())
 
-        resp = "HTTP/1.1 404 Not Found\r\n"+response_headers_raw+"\r\n"+msg
+        resp = "HTTP/1.1 404 Not Found\r\n"+response_headers_raw+"\r\n"+msg_raw
         resp_bytes = bytes(resp, "utf-8")
         print(resp_bytes)
         self.request.sendall(resp_bytes)
