4f36b02cf28cb989b31138902d53b6f7cca16f66
ajh123 <41990982+ajh123@users.noreply.github.com>
2022-12-02 11:43:16 +0000

    use 404

--- other/15.py
+++ other/15.py
@@ -5,7 +5,8 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
         # just send back the same data, but upper-cased
-        msg = "<h1>Hello, World!</h1>"
+        msg = "<h1>404 Not Found</h1>"
+        msg += "<p>The server did not find the file requested.</p><hr><center><p>pyServe 0.0.1</p></center>"
         response_headers = {
             'Content-Type': 'text/html; encoding=utf8',
             'Content-Length': len(msg),
@@ -14,7 +15,7 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 
         response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
 
-        resp = "HTTP/1.1 200 OK\r\n"+response_headers_raw+"\r\n"+msg
+        resp = "HTTP/1.1 404 Not Found\r\n"+response_headers_raw+"\r\n"+msg
         resp_bytes = bytes(resp, "utf-8")
         print(resp_bytes)
         self.request.sendall(resp_bytes)
