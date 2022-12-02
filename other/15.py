04de8faea72a97be64e867a7d5e4128fc2be1063
ajh123 <41990982+ajh123@users.noreply.github.com>
2022-12-02 10:23:02 +0000

    Add a simple http server

---
+++ other/15.py
@@ -0,0 +1,26 @@
+import socketserver
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        # just send back the same data, but upper-cased
+        msg = "<h1>Hello, World!</h1>"
+        response_headers = {
+            'Content-Type': 'text/html; encoding=utf8',
+            'Content-Length': len(msg),
+            'Connection': 'close',
+        }
+
+        response_headers_raw = ''.join('%s: %s\r\n' % (k, v) for k, v in response_headers.items())
+
+        resp = "HTTP/1.1 200 OK\r\n"+response_headers_raw+"\r\n"+msg
+        resp_bytes = bytes(resp, "utf-8")
+        print(resp_bytes)
+        self.request.sendall(resp_bytes)
+
+if __name__ == "__main__":
+    HOST, PORT = "0.0.0.0", 9999
+    print(f"Running server om {HOST}:{PORT}")
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        server.serve_forever()
