aec06155f7e392b2f4bed7a0cfd88ddf345aedd9
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2023-10-05 13:53:27 +0000

    Test

--- y2/binarySearch/binarySearch.py
+++ y2/binarySearch/binarySearch.py
@@ -1,10 +1,18 @@
+debug = 0
+# Instructions (worst case) = 8
 def binary_search(arr, low, high, x):
-    if high >= low:
-        mid = (high + low) // 2
-        if arr[mid] == x:
+    global debug # Debug only
+    # print("x")
+    if high >= low: # + 1
+        debug =+ 1 # Debug only
+        mid = (high + low) // 2 # + 3
+        debug =+ 1 # Debug only
+        if arr[mid] == x: # + 1
             return mid
-        elif arr[mid] < x:
-            return binary_search(arr, mid+1, high, x)
+        elif arr[mid] < x: # + 1
+            # print("y")
+            debug =+ 1 + 2 # Debug only
+            return binary_search(arr, mid+1, high, x) # + 2
         else:
             return binary_search(arr, low, mid-1, x)
     else:
@@ -13,6 +21,8 @@ def binary_search(arr, low, high, x):
 arr = [2, 3, 4, 10, 40]
 print(f"The list is {arr}\n")
 numberToFind = int(input("Enter number to find\n"))
+
+print(f"debug={debug}")
 
 result = binary_search(arr, 0, len(arr)-1, numberToFind)
 
